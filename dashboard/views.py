e11cb215fdc187c77aa57aa37b4f3e44374dc233
FinEredium1 <fahmed71@gatech.edu>
2025-04-12 17:03:10 -0400

    Added basic dashboard functionalities without AI parts

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Goal
 
-# Create your views here.
+
+def dashboard(request):
+    if request.method == 'POST':
+        if 'submit_goal' in request.POST:
+            text = request.POST.get('user_input_field')
+            if text:
+                Goal.objects.create(text=text, user=request.user)
+
+        elif 'complete_goal' in request.POST:
+            goal_id = request.POST.get('goal_id')
+            goal = Goal.objects.get(id=goal_id)
+            goal.completed = True
+            goal.abandoned = False
+            goal.save()
+            return redirect('Dashboard')
+
+        elif 'abandon_goal' in request.POST:
+            goal_id = request.POST.get('goal_id')
+            goal = Goal.objects.get(id=goal_id)
+            goal.abandoned = True
+            goal.completed = False
+            goal.save()
+            return redirect('Dashboard')
+
+    all_goals = Goal.objects.filter(user=request.user).order_by('created_at')
+    completed_goals_count = Goal.objects.filter(user=request.user, completed=True).count()
+    abandoned_goals_count = Goal.objects.filter(user=request.user, abandoned=True).count()
+    remaining_goals_count = Goal.objects.filter(user=request.user, completed=False, abandoned=False).count()
+
+    return render(request, 'dashboard/goals.html', {
+        'user_inputs': all_goals,
+        'completed_goals_count': completed_goals_count,
+        'abandoned_goals_count': abandoned_goals_count,
+        'remaining_goals_count': remaining_goals_count,
+    })

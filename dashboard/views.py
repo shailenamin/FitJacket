0cdd6476bb728cd387d7441c854b134428e4ddb0
FinEredium1 <fahmed71@gatech.edu>
2025-04-12 20:21:42 -0400

    minor design changes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Goal
 
 
+@login_required
 def dashboard(request):
     if request.method == 'POST':
         if 'submit_goal' in request.POST:
@@ -25,14 +27,25 @@ def dashboard(request):
             goal.save()
             return redirect('Dashboard')
 
-    all_goals = Goal.objects.filter(user=request.user).order_by('created_at')
+    current_goals = Goal.objects.filter(user=request.user, completed=False, abandoned=False)
     completed_goals_count = Goal.objects.filter(user=request.user, completed=True).count()
     abandoned_goals_count = Goal.objects.filter(user=request.user, abandoned=True).count()
     remaining_goals_count = Goal.objects.filter(user=request.user, completed=False, abandoned=False).count()
 
     return render(request, 'dashboard/goals.html', {
-        'user_inputs': all_goals,
+        'user_inputs': current_goals,
         'completed_goals_count': completed_goals_count,
         'abandoned_goals_count': abandoned_goals_count,
         'remaining_goals_count': remaining_goals_count,
+    })
+
+
+@login_required
+def goal_history(request):
+    completed_goals = Goal.objects.filter(user=request.user, completed=True)
+    abandoned_goals = Goal.objects.filter(user=request.user, abandoned=True)
+
+    return render(request, 'dashboard/goal-history.html', {
+        'completed_goals': completed_goals,
+        'abandoned_goals': abandoned_goals,
     })

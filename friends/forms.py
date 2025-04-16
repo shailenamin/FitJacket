2457d22647ca85365c7b7f81290438be67c7bbfd
Toxicl-16 <34041366+Toxicl-16@users.noreply.github.com>
2025-04-15 20:40:34 -0400

    Added workout Groups

--- friends/forms.py
+++ friends/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from .models import WorkoutGroup
 
 class FriendMgmtForm(forms.Form):
     friend = forms.CharField(max_length=100,required=False)
+
+class WorkoutGroupForm(forms.ModelForm):
+    class Meta:
+        model = WorkoutGroup
+        fields = ['name']
--- friends/views.py
+++ friends/views.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import FriendRequest, Friendship
+from .models import FriendRequest, Friendship, WorkoutGroup, GroupInvite, GroupMember
 from django.db.models import Q
 from django.core.mail import send_mail
+from .forms import WorkoutGroupForm
 
 @login_required
 def users(request):
     users = User.objects.exclude(id=request.user.id)
+    groups = WorkoutGroup.objects.filter(groupmember__user=request.user)
     sent_requests = FriendRequest.objects.filter(from_user=request.user).values_list('to_user_id', flat=True)
     received_requests = FriendRequest.objects.filter(to_user=request.user)
     friendships = Friendship.objects.filter(Q(user1=request.user) | Q(user2=request.user))
@@ -17,7 +19,7 @@ def users(request):
             friend_ids.add(friendship.user2.id)
         else:
             friend_ids.add(friendship.user1.id)
-    return render(request, 'friends/userList.html', {'users': users,'sent_requests': sent_requests,'received_requests': received_requests,'friends': friend_ids,})
+    return render(request, 'friends/userList.html', {'users': users,'sent_requests': sent_requests,'received_requests': received_requests,'friends': friend_ids, 'groups': groups,})
 
 @login_required
 def sendRequest(request, user_id):
@@ -53,4 +55,48 @@ def removeFriend(request, user_id):
     friendship = Friendship.objects.filter((Q(user1=request.user) & Q(user2=other_user)) | (Q(user1=other_user) & Q(user2=request.user))).first()
     if friendship:
         friendship.delete()
+    return redirect('friends:userList')
+
+@login_required
+def create_group(request):
+    if request.method == 'POST':
+        form = WorkoutGroupForm(request.POST)
+        if form.is_valid():
+            group = form.save(commit=False)
+            group.created_by = request.user
+            group.save()
+            GroupMember.objects.create(group=group, user=request.user)
+            return redirect('friends:userList')
+    else:
+        form = WorkoutGroupForm()
+    return render(request, 'friends/create_group.html', {'form': form})
+
+@login_required
+def invite_to_group(request, group_id=None, user_id=None):
+    if request.method == 'POST':
+        group_id = request.POST.get('group_id')
+    group = get_object_or_404(WorkoutGroup, id=group_id)
+    to_user = get_object_or_404(User, id=user_id)
+    if not GroupMember.objects.filter(group=group, user=request.user).exists():
+        return redirect('friends:userList')
+    GroupInvite.objects.get_or_create(group=group, from_user=request.user, to_user=to_user)
+    return redirect('friends:userList')
+
+def respond_group_invite(request, invite_id):
+    invite = get_object_or_404(GroupInvite, id=invite_id, to_user=request.user)
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        if action == 'accept':
+            GroupMember.objects.get_or_create(group=invite.group, user=request.user)
+            invite.accepted = True
+            invite.save()
+        invite.delete()
+    return redirect('friends:userList')
+
+@login_required
+def leave_group(request, group_id):
+    group = get_object_or_404(WorkoutGroup, id=group_id)
+    membership = GroupMember.objects.filter(group=group, user=request.user).first()
+    if membership:
+        membership.delete()
     return redirect('friends:userList')

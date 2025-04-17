e30d60193b56aee12f758cf3bfbf802ea62e7717
Toxicl-16 <34041366+Toxicl-16@users.noreply.github.com>
2025-04-17 16:36:06 -0400

    Created Event Add/List

--- events/models.py
+++ events/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_event')
+    created_at = models.DateTimeField(auto_now_add=True)
--- events/views.py
+++ events/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Event
+from django.db.models import Q
+from django.core.mail import send_mail
+from .forms import EventForm
 # Create your views here.
 
 def about(request):
     template_data = {}
     template_data['title'] = 'About'
-    return render(request, 'events/events.html', {'template_data': template_data})
+    events = Event.objects.filter()
+    return render(request, 'events/events.html', {'template_data': template_data, 'events': events,})
+
+@login_required
+def create_group(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            group = form.save(commit=False)
+            group.created_by = request.user
+            group.save()
+            return redirect('events.about')
+    else:
+        form = EventForm()
+    return render(request, 'events/create_event.html', {'form': form})

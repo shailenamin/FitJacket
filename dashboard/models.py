7f78c5e029745d1725347d4607921812d5fe0ff8
Payton-6 <psilverman6@gatech.edu>
2025-04-25 12:26:06 -0400

    Update models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.utils.timezone import localtime
 
 
 class Goal(models.Model):

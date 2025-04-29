62ed8a059975832a7f07ea3c9a51bd59f328eb65
Toxicl-16 <34041366+Toxicl-16@users.noreply.github.com>
2025-04-28 22:17:56 -0400

    Workout Challenges

--- friends/forms.py
+++ friends/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import WorkoutGroup
+from .models import WorkoutGroup, Challenge
 
 class FriendMgmtForm(forms.Form):
     friend = forms.CharField(max_length=100,required=False)
@@ -8,3 +8,15 @@ class WorkoutGroupForm(forms.ModelForm):
     class Meta:
         model = WorkoutGroup
         fields = ['name']
+
+WORKOUT_TYPES = [
+        ('Strength', 'Strength'),
+        ('Cardio',   'Cardio'),
+        ('Yoga',     'Yoga'),
+    ]
+class ChallengeForm(forms.ModelForm):
+    workout_type = forms.ChoiceField(choices=WORKOUT_TYPES)
+    class Meta:
+        model = Challenge
+        fields = ['name', 'workout_type', 'description', 'challenge_end_date']
+        widgets = {'challenge_end_date': forms.DateInput(attrs={'type': 'date'}),}

9d4ce195d857c655d7e2b31b1b11ac0277062cf1
VanshN123 <vanshnishchal86@gmail.com>
2025-04-29 01:09:46 -0400

    Email for streak
    (cherry picked from commit 41ffb860d89c84195ba9362182af5dd855747d68)

--- administratorPanel/urls.py
+++ administratorPanel/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('admin-panel/', views.emailUsers, name='administratorPanel.emailUsers'),
+    path('warn-streaks/', views.warnUsersAboutStreaks, name='warn-streaks'),
+
 ]
